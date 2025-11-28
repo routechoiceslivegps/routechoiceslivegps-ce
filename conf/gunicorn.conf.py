70ac17c1223aa3ee750d29f38e40ffbc6763a469
Rapha <1859643+rphlo@users.noreply.github.com>
2025-11-28 10:03:17 +0200

    Update gunicorn.conf.py

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -1,7 +1,7 @@
-wsgi_app = "routechoices.wsgi:application"
+wsgi_app = "routechoices.bin.wsgi:application"
 preload_app = True
 daemon = False
-raw_env = ["DJANGO_SETTINGS_MODULE=routechoices.conf.settings"]
+raw_env = ["DJANGO_SETTINGS_MODULE=routechoices.settings"]
 workers = 3
 threads = 3
 max_requests = 3000
